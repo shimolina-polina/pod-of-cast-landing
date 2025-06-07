9859d82e0730b363a23e4a8540e0cf4261505b95
shimolina-polina <shimolinapk@gmail.com>
2025-06-07 15:56:58 +0300

    fix: add icons to supported

--- src/components/Header/Supported/Supported.tsx
+++ src/components/Header/Supported/Supported.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components"
+import youtube from '/Youtube.svg'
+import googlePodcasts from '/GooglePodcast.svg' 
+import spotify from '/Spotify.svg'
 
 const StyledItem = styled.div`
     width: 275px;
@@ -10,14 +13,25 @@ const StyledItem = styled.div`
     font-weight: bold;
 `
 
+const StyledIconItem = styled.div`
+    display: flex;
+    align-items: center;
+`
+
 
 export default function Supported () {
     return (
-        <div style={{marginTop: '120px', maxWidth: '1160px', width: '100%', height: '124px', borderTop: '1px solid black', borderBottom: '1px solid black', display: 'flex', gap: '20px', alignItems: 'center'}}>
+        <div style={{marginTop: '120px', maxWidth: '1160px', width: '100%', height: '124px', borderTop: '1px solid black', borderBottom: '1.5px solid black', display: 'flex', gap: '20px', alignItems: 'center'}}>
             <StyledItem>Supported by:</StyledItem>
-            <StyledItem>Spotify</StyledItem>
-            <StyledItem>GooglePodcasts</StyledItem>
-            <StyledItem>YouTube</StyledItem>
+            <StyledIconItem style={{padding: '25px 73.5px'}}>
+                <img src={spotify} alt={"Youtube"} style={{width: '128px', height: '34px'}}/>
+            </StyledIconItem >
+            <StyledIconItem  style={{padding: '24px 38.5px'}}>
+                <img src={googlePodcasts} alt={"Youtube"} style={{width: '198px', height: '36px'}}/>
+            </StyledIconItem>
+            <StyledIconItem  style={{padding: '28px 75.5px'}}>
+                <img src={youtube} alt={"Youtube"} style={{width: '124px', height: '28px'}}/>
+            </StyledIconItem>
         </div>
     )
 }
